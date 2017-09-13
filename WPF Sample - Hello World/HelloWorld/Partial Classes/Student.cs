79262f5f50826483a8c53db606e682dcfa495c54
Raluca-PC <banescu.raluca13@gmail.com>
2017-09-13 15:19:10 +0300

    update

---
+++ WPF Sample - Hello World/HelloWorld/Partial Classes/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Partial_Classes
+{
+    public partial class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ WPF Sample - Hello World/HelloWorld/Partial Classes/StudentMedicalData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Partial_Classes
+{
+    public partial class Student
+    {
+        private int medicalRecordId;
+
+        public Student(string firstName, string lastName)
+        {
+            medicalRecordId = -1;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
